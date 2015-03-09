7f29e9801d9eae335871b6f025465f0d10b76cad
Miguel Mendez <keloxers@gmail.com>
2015-03-09 19:38:02 -0300

    uploads 1

---
+++ app/lang/es/app.php
@@ -0,0 +1,47 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| User Repositiory Messages
+	|--------------------------------------------------------------------------
+	*/
+
+	'site' 		=>  "SyNMisiones.com",
+
+	'title_site'		=>	"S&N Misiones",
+
+	'title_home' => "Consultora de Gestión de Calidad",
+
+	'email'		=>	"<strong>comercial@synmisiones.com</strong>",
+
+	'telefono'		=>	"<strong>+54 0376 4439326</strong>",
+
+	'seguinos_facebook'		=>	"https://facebook.com/pages/SN-Servicios/1564714993750159?ref=hl S&N Servicios",
+
+	'seguinos_twitter'		=>	"https://twitter.com/synservicios",
+
+	'seguinos_googleplus'		=>	"http://plus.google.com/u/0/116409445805080169378/posts",
+
+	'seguinos_instagram'		=>	"",
+
+	'seguinos_youtube'		=>	"http://www.youtube.com/channel/UCWTCkRQ6MUXuSK1TXp0GI_w",
+
+	'direccion'		=>	"Posadas, Misiones AR",
+
+	'contacto'		=>	"+54 0376 4439326 <br><a href='mailto:info@synmisiones.com'>info@synmisiones.com</a>",
+
+	'title_food' => "S&N Misiones",
+
+''		=>	"",
+''		=>	"",
+''		=>	"",
+''		=>	"",
+''		=>	"",
+''		=>	"",
+
+
+
+
+);
